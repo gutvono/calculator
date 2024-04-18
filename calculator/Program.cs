9118a530b1f5c2ef7f5589cc4736ea0130a6fed2
Gustavo <guvono4@gmail.com>
2024-04-18 18:53:32 -0300

    feat: user can choose the operation he wants
    - user can choose operator;
    - output changes for better user experience;
    - this update already have the operator validators;

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿float n1 = 0, n2 = 0;
+int operacao = 0, aux = 0;
 
 Console.Write("Digite o primeiro numero:");
 n1 = float.Parse(Console.ReadLine());
@@ -6,21 +7,60 @@ n1 = float.Parse(Console.ReadLine());
 Console.Write("Digite o segundo numero:");
 n2 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Soma: " + (n1 + n2));
-Console.WriteLine("Subtracao: " + (n1 - n2));
-Console.WriteLine("Multiplicacao: " + (n1 * n2));
-if (n2 == 0)
+do
 {
-    Console.WriteLine("Impossível dividir por 0");
+    Console.WriteLine($"Qual operação você quer realizar com os números {n1} e {n2}?");
+    Console.WriteLine("1 - SOMA");
+    Console.WriteLine("2 - SUBTRAIR");
+    Console.WriteLine("3 - MULTIPLICAR");
+    Console.WriteLine("4 - DIVIDIR");
+
+    if (aux > 0)
+    {
+        Console.WriteLine("!--------------------------------------------!");
+        Console.WriteLine("ERRO: Operação inválida!");
+        Console.WriteLine("!--------------------------------------------!");
+    }
+
+    operacao = int.Parse(Console.ReadLine());
+    aux++;
+} while (operacao < 1 && operacao > 4);
+
+
+Console.WriteLine();
+
+if (operacao == 1)
+{
+    Console.WriteLine($"Soma: {n1} + {n2} = {n1 + n2}");
+}
+else if (operacao == 2)
+{
+    Console.WriteLine($"Subtracao: {n1} - {n2} = {n1 - n2}");
+}
+else if (operacao == 3)
+{
+    Console.WriteLine($"Multiplicacao: {n1} x {n2} = {n1 * n2}");
 }
 else
 {
-    if (n2 < 0)
+    if (n2 == 0)
     {
-        Console.WriteLine("Número negativo");
+        Console.WriteLine("Impossível dividir por 0");
     }
     else
     {
-        Console.WriteLine("Divisao: " + (n1 / n2));
+        if (n2 < 0)
+        {
+            Console.WriteLine("Número negativo");
+        }
+        else
+        {
+            Console.WriteLine($"Divisao: {n1} / {n2} = {n1 / n2}");
+        }
     }
 }
+
+Console.WriteLine();
+
+Console.WriteLine("Aperte qualquer tecla para terminar o programa.");
+Console.ReadKey();
