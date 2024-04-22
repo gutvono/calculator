85117339fc30fd560f4eb554fefff0467030b93b
Gustavo <guvono4@gmail.com>
2024-04-22 11:11:09 -0300

    update: now the user needs do choose 4 numbers instead 2

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,11 +1,43 @@
-﻿float n1 = 0, n2 = 0;
+﻿float n1 = 0, n2 = 0, n3 = 0, n4 = 0;
 int operacao = 0;
+
+Console.WriteLine("BEM VINDO AO APP CALCULATOR");
 
 Console.Write("Digite o primeiro numero:");
 n1 = float.Parse(Console.ReadLine());
 
-Console.Write("Digite o segundo numero:");
-n2 = float.Parse(Console.ReadLine());
+do
+{
+    Console.Write("Digite o segundo numero:");
+    n2 = float.Parse(Console.ReadLine());
+
+    if (n2 == 0)
+    {
+        Console.WriteLine("Informe um número diferente de 0.");
+    }
+} while (n2 == 0);
+
+do
+{
+    Console.Write("Digite o terceiro numero:");
+    n3 = float.Parse(Console.ReadLine());
+
+    if (n3 == 0)
+    {
+        Console.WriteLine("Informe um número diferente de 0.");
+    }
+} while (n3 == 0);
+
+do
+{
+    Console.Write("Digite o quarto numero:");
+    n4 = float.Parse(Console.ReadLine());
+
+    if (n4 == 0)
+    {
+        Console.WriteLine("Informe um número diferente de 0.");
+    }
+} while (n4 == 0);
 
 do
 {
@@ -15,7 +47,7 @@ do
     Console.WriteLine("3 - MULTIPLICAR");
     Console.WriteLine("4 - DIVIDIR");
     operacao = int.Parse(Console.ReadLine());
-    
+
     Console.Clear();
 
     if (operacao < 1 || operacao > 4)
@@ -26,38 +58,23 @@ do
     }
 } while (operacao < 1 || operacao > 4);
 
-
 Console.WriteLine();
 
 if (operacao == 1)
 {
-    Console.WriteLine($"Soma: {n1} + {n2} = {n1 + n2}");
+    Console.WriteLine($"Soma: {n1} + {n2} + {n3} + {n4} = {n1 + n2 + n3 + n4}");
 }
 else if (operacao == 2)
 {
-    Console.WriteLine($"Subtracao: {n1} - {n2} = {n1 - n2}");
+    Console.WriteLine($"Subtracao: {n1} - {n2} - {n3} - {n4} = {n1 - n2 - n3 - n4}");
 }
 else if (operacao == 3)
 {
-    Console.WriteLine($"Multiplicacao: {n1} x {n2} = {n1 * n2}");
+    Console.WriteLine($"Multiplicacao: {n1} x {n2} x {n3} x {n4} = {n1 * n2 * n3 * n4}");
 }
 else
 {
-    if (n2 == 0)
-    {
-        Console.WriteLine("Impossível dividir por 0");
-    }
-    else
-    {
-        if (n2 < 0)
-        {
-            Console.WriteLine("Número negativo");
-        }
-        else
-        {
-            Console.WriteLine($"Divisao: {n1} / {n2} = {n1 / n2}");
-        }
-    }
+    Console.WriteLine($"Divisao: {n1} / {n2} / {n3} / {n4} = {n1 / n2 / n3 / n4}");
 }
 
 Console.WriteLine();
