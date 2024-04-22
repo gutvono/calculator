09daab45d5e780f1e6e78d53f5e32c25da6c364c
Gustavo <guvono4@gmail.com>
2024-04-22 11:42:38 -0300

    update: now the operation algorythm use switch case

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,15 +1,15 @@
-﻿float n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+﻿double n1 = 0, n2 = 0, n3 = 0, n4 = 0;
 int operacao = 0;
 
 Console.WriteLine("BEM VINDO AO APP CALCULATOR");
 
 Console.Write("Digite o primeiro numero:");
-n1 = float.Parse(Console.ReadLine());
+n1 = double.Parse(Console.ReadLine());
 
 do
 {
     Console.Write("Digite o segundo numero:");
-    n2 = float.Parse(Console.ReadLine());
+    n2 = double.Parse(Console.ReadLine());
 
     if (n2 == 0)
     {
@@ -20,7 +20,7 @@ do
 do
 {
     Console.Write("Digite o terceiro numero:");
-    n3 = float.Parse(Console.ReadLine());
+    n3 = double.Parse(Console.ReadLine());
 
     if (n3 == 0)
     {
@@ -31,7 +31,7 @@ do
 do
 {
     Console.Write("Digite o quarto numero:");
-    n4 = float.Parse(Console.ReadLine());
+    n4 = double.Parse(Console.ReadLine());
 
     if (n4 == 0)
     {
@@ -49,35 +49,29 @@ do
     operacao = int.Parse(Console.ReadLine());
 
     Console.Clear();
+    Console.WriteLine();
 
-    if (operacao < 1 || operacao > 4)
+    switch (operacao)
     {
-        Console.WriteLine("!--------------------------------------------!");
-        Console.WriteLine("ERRO: Operação inválida!");
-        Console.WriteLine("!--------------------------------------------!");
+        case 1:
+            Console.WriteLine($"Soma: {n1} + {n2} + {n3} + {n4} = {n1 + n2 + n3 + n4}");
+            break;
+        case 2:
+            Console.WriteLine($"Subtracao: {n1} - {n2} - {n3} - {n4} = {n1 - n2 - n3 - n4}");
+            break;
+        case 3:
+            Console.WriteLine($"Multiplicacao: {n1} x {n2} x {n3} x {n4} = {n1 * n2 * n3 * n4}");
+            break;
+        case 4:
+            Console.WriteLine($"Divisao: {n1} / {n2} / {n3} / {n4} = {n1 / n2 / n3 / n4}");
+            break;
+        default:
+            Console.WriteLine("!--------------------------------------------!");
+            Console.WriteLine("ERRO: Operação inválida!");
+            Console.WriteLine("!--------------------------------------------!");
+            break;
     }
 } while (operacao < 1 || operacao > 4);
 
-Console.WriteLine();
-
-if (operacao == 1)
-{
-    Console.WriteLine($"Soma: {n1} + {n2} + {n3} + {n4} = {n1 + n2 + n3 + n4}");
-}
-else if (operacao == 2)
-{
-    Console.WriteLine($"Subtracao: {n1} - {n2} - {n3} - {n4} = {n1 - n2 - n3 - n4}");
-}
-else if (operacao == 3)
-{
-    Console.WriteLine($"Multiplicacao: {n1} x {n2} x {n3} x {n4} = {n1 * n2 * n3 * n4}");
-}
-else
-{
-    Console.WriteLine($"Divisao: {n1} / {n2} / {n3} / {n4} = {n1 / n2 / n3 / n4}");
-}
-
-Console.WriteLine();
-
-Console.WriteLine("Pressione qualquer tecla para terminar o programa.");
+Console.WriteLine("\nPressione qualquer tecla para terminar o programa.");
 Console.ReadKey();
